90808af3b8a81c7e66a87092b455841ea0345406
Benjamin Odoi-Lartey <odlabenonline@gmail.com>
2025-11-08 10:32:33 -0500

    Addition of day 8 studies Basically on Functinos and Inputs

---
+++ day008/day8_coding_challenge.py
@@ -0,0 +1,38 @@
+# # #Write your code below this line
+# # import math
+# # def paint_calc(height, width, cover):
+# #     cans = math.ceil((height * width)/5)
+# #     print(f"You will need {cans} can(s) to finish painting")
+# #
+# #
+# # #Write your code above this line
+# #
+# # #Don't change the code below
+# # test_h = int(input("Height of wall: \n"))
+# # test_w = int(input("Width of wall: \n"))
+# # coverage = 5
+# # paint_calc(height = test_h, width = test_w, cover = coverage)
+#
+# ########### PRIME NUMBER CHECKER ###########
+# #Number that is only divisible by 2 numbers, 1 and the number itself
+# #Write your code below this line
+#
+# def prime_checker(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#     if is_prime:
+#         print("It's a prime number")
+#     else:
+#         print("It's not a prime number")
+#
+# #It's a prime number.
+# #It's not a prime  number.
+#
+#
+# #Write your code above this line
+#
+# #Do NOT change any of the code below
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
---
+++ day008/day8_main.py
@@ -0,0 +1,44 @@
+# # # Review:
+# # # Create a function called greet().
+# # # Write 3 print statements inside the function.
+# # # Call the greet() function and run your code.
+# #
+# # # #Simple Function
+# # # def greet():
+# # #     print("Hello World")
+# # #     print("I just created a function")
+# # #     print("...and I called it to execute; Thanks!")
+# # #
+# # # greet()
+# #
+# #
+# # #Function that allows for input
+# #
+# # def greet_with_name(name): #Name in the function is argument
+# #     print(f"Hello {name}")
+# #     print(f"How do you do {name}")
+# #
+# # name = input("Please what is your name: \n")
+# # greet_with_name(name)
+# # from Day003.day3_love_calculator import name1
+#
+#
+# #Functions with more than 1 input
+# def greet_with(name, location):
+#     print(f"hello {name}")
+#     print(f"What is it like in {location}")
+#
+# name = input("Please enter your name\n")
+# location = input("Please enter your city\n")
+#
+# greet_with(name, location)
+
+#Functions with Keyword Arguments
+def greet_with(name, location):
+    print(f"hello {name}")
+    print(f"What is it like in {location}")
+
+username = input("Please enter your name\n")
+city = input("Please enter your city\n")
+
+greet_with(location = city, name = username) #Keyword Arguments, position doesn't matter
