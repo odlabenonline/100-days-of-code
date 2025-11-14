6f6df5b3620337a28d1c899adc16f6f007117c64
Benjamin Odoi-Lartey <odlabenonline@gmail.com>
2025-11-13 22:01:35 -0500

    Upload of Day 10 files

---
+++ day010/day10_main.py
@@ -0,0 +1,23 @@
+# # #Functions with Outputs
+# #
+# # def my_function():
+# #     result = 3 * 2
+# #     return result
+# #
+# # my_function()
+#
+#
+def format_name(f_name, l_name):
+    # print(f_name.title())
+    # print(l_name.title())
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    #print(f"{formated_f_name} {formated_l_name}")
+    return f"{formated_f_name} {formated_l_name}"
+
+formated_string =   format_name("AnGELa", "yU")
+print(formated_string)
+
+#
+
+#### DOCSTRINGS ################
