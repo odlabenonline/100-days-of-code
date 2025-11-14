767febf4f542e2f920223fb4e27d0e4a18f08a88
Benjamin Odoi-Lartey <odlabenonline@gmail.com>
2025-11-13 22:05:38 -0500

    Update of day13 files

---
+++ day013/day13_coding_challenge.py
@@ -0,0 +1,24 @@
+# number = int(input("Which number do you want to check?"))
+#
+# if number % 2 == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+#
+#
+#
+#
+#
+####### LEAP YEAR ##############
+year = int(input("Which year do you want to check?f"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year.")
+        else:
+            print("Not Leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
