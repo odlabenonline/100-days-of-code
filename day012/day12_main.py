26c96a915704c02b26332d6eef0a3131285a4c8a
Benjamin Odoi-Lartey <odlabenonline@gmail.com>
2025-11-13 22:04:26 -0500

    Upload of day 12 file

---
+++ day012/day12_main.py
@@ -0,0 +1,25 @@
+############## SCOPE ##################
+
+enemies = 1
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemeies inside function: {enemies}")
+
+increase_enemies()
+print(f"'enemies outside function: {enemies}")
+
+######## Local Scope #####################
+
+
+
+
+
+
+######### No Block Scope in Python ##########
+
+
+
+
+
+############## Modifying Global Scope ###########
