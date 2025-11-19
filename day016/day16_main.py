c7705cd4b26ff97ae451c9244228d57cb211410f
Benjamin Odoi-Lartey <odlabenonline@gmail.com>
2025-11-18 22:06:21 -0500

    Upload of day016 resources from local to github OOP: Modules, attributes, methods Pocedural  | DotNotation | Documentation reading | Python Packages | Pypi.org | pip install etc Constructing Objects
    Files included are 
    another_module.py
    coffee_maker.py
    day16_main.py
    main.py
    menu.py
    money_machine.py

---
+++ day016/day16_main.py
@@ -0,0 +1,22 @@
+#  # import turtle #read about turtle module
+# # timmy = turtle.Turtle()
+#
+# ### The above could also go like
+#
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy) #Console will show creation of object timmy in specific memory location
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvaheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable() #A new object table is created from teh PrettyTable class within the prettytable library
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l" #attribute
+# print(table)
