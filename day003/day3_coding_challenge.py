4cca93dc01c84603756071bb93ad62cf4b038e3c
Benjamin Odoi-Lartey <odlabenonline@gmail.com>
2025-11-24 22:09:03 -0500

    N/A

--- day003/day3_coding_challenge.py
+++ day003/day3_coding_challenge.py
@@ -26,27 +26,27 @@
 # else:
 #     print(f"The number you entered is ${number} and it is ODD")
 #
-#
-# # ########### BMI with INTERPRETATION ########################
-# # #Don't change the code below
-# # height = float(input("Enter your heigh in m: "))
-# # weight = float(input("Enter your weight in kg: "))
-# # #Don't change the code above
-# #
-# # #Write your code below this line
-# # bmi = round(weight / height ** 2, 2)
-# # if bmi < 18.5:
-# #     print(f"Your BMI is {bmi} and you are UNDERWEIGHT")
-# # elif bmi < 25:
-# #     print(f"Your BMI is {bmi} and you have NORMAL WEIGHT")
-# # elif bmi < 30:
-# #     print(f"Your BMI is {bmi} and you are OVERWEIGHT")
-# # elif bmi < 35:
-# #     print(f"Your BMI is {bmi} and you are OBESE")
-# # else:
-# #     print(f"Your BMI is {bmi} and you are CLINICALLY OBESE")
-#
-#
+
+# ########### BMI with INTERPRETATION ########################
+# #Don't change the code below
+# height = float(input("Enter your heigh in m: "))
+# weight = float(input("Enter your weight in kg: "))
+# #Don't change the code above
+
+# #Write your code below this line
+# bmi = round(weight / height ** 2, 2)
+# if bmi < 18.5:
+#     print(f"Your BMI is {bmi} and you are UNDERWEIGHT")
+# elif bmi < 25:
+#     print(f"Your BMI is {bmi} and you have NORMAL WEIGHT")
+# elif bmi < 30:
+#     print(f"Your BMI is {bmi} and you are OVERWEIGHT")
+# elif bmi < 35:
+#     print(f"Your BMI is {bmi} and you are OBESE")
+# else:
+#     print(f"Your BMI is {bmi} and you are CLINICALLY OBESE")
+
+
 # # #################### LEAP YEAR CHALLENGE #######################
 # # #Don't change the code below
 # # year = int(input("Which year do you want to check"))
