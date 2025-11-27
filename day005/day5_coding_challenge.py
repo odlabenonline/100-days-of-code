947fb057da60119e08eb346fc73f748a0b3bf33d
odlabenonline <odlabenonline@gmail.com>
2025-11-26 23:45:33 -0500

    Revisited and updated day005

--- day005/day5_coding_challenge.py
+++ day005/day5_coding_challenge.py
@@ -73,12 +73,36 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password"\n))
-nr_symbols = int(input("How many symbols would you like"\n))
-nr_numbers = int(input("How many numbers would you like"\n))
+nr_letters = int(input("How many letters would you like in your password\n"))
+nr_symbols = int(input("How many symbols would you like\n"))
+nr_numbers = int(input("How many numbers would you like\n"))
 
 #Eazy Level (Sequenced LettersSymbolsNumbers
+# password = ""
 
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(letters)
 
+# for char in range(1, nr_symbols + 1):
+#     password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#     password += random.choice(numbers)
+
+#print(password)
 
 #Hard Level (RandomizedMixed)
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+    password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
