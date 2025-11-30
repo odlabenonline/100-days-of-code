81c3afcd4885bde8224f277729fdf026780ee717
odlabenonline <odlabenonline@gmail.com>
2025-11-29 19:45:00 -0500

    Updating Day 6 with reebog examples (whileLoop, for loop, if statements

--- day006/day6_code_challenge.py
+++ day006/day6_code_challenge.py
@@ -1,52 +1,129 @@
-# Karel the robot - Exercise
-
-
-# Move ROBOT to draw a square using the resource at
-# https://reeborg.ca/reeborg.html
-# Coding Exercise: Challenge : Code for the Robot to jump some hurdles
+# # # # Karel the robot - Exercise
+# # #
+# # #
+# # # # Move ROBOT to draw a square using the resource at
+# # # # https://reeborg.ca/reeborg.html
+# # # # Coding Exercise: Challenge : Code for the Robot to jump some hurdles
+# # # #
+# # # #
+# # # # def jump_hurdle():
+# # # #     turn_left()
+# # # #     turn_left()
+# # # #     turn_left()
+# # # #     move()
+# # # #     turn_left()
+# # # #     turn_left()
+# # # #     turn_left()
+# # # #     move()
+# # # #     turn_left()
+# # # #
+# # # #
+# # # # for i in range(1, 10):
+# # # #     move()
+# # # #     turn_left()
+# # # #     move()
+# # # #     jump_hurdle()
+# # #
+# # # #
+# # # # ############ RE WRITTEN CODE to include 2 functions ######
+# # # # def turn_right():
+# # # #     turn_left()
+# # # #     turn_left()
+# # # #     turn_left()
+# # # #
+# # # #
+# # # # def jump_hurdle():
+# # # #     turn_right()
+# # # #     move()
+# # # #     turn_right()
+# # # #     move()
+# # # #     turn_left()
+# # # #
+# # # #
+# # # # for i in range(1, 7):
+# # # #     move()
+# # # #     turn_left()
+# # # #     move()
+# # # #     jump_hurdle()
+# # # #
+# # # # IN THE ABSENCE OF FOR LOOP, we could use WHILE LOOP as defined below
+# # # # Loot into resource for solution
+# # #
+# # #
+# # # ############# HURDLE 2 EXAMPLE in reeborg #######
+# # #
+# # # def turn_right():
+# # #     turn_left()
+# # #     turn_left()
+# # #     turn_left()
+# # #
+# # #
+# # # def jump():
+# # #     move()
+# # #     turn_left()
+# # #     move()
+# # #     turn_right()
+# # #     move()
+# # #     turn_right()
+# # #     move()
+# # #     turn_left()
+# # #
+# # #
+# # # while not at_goal():
+# # #     jump()
+# # # print("FINISHED!")
+# # # #
+# # # #
+# #
+# # ##################### HURDLE 3 ###############
+# # def turn_right():
+# #     turn_left()
+# #     turn_left()
+# #     turn_left()
+# #
+# #
+# # def jump():
+# #     turn_left()
+# #     move()
+# #     turn_right()
+# #     move()
+# #     turn_right()
+# #     move()
+# #     turn_left()
+# #
+# #
+# # while not at_goal():
+# #     if wall_in_front():
+# #         jump()
+# #     else:
+# #         move()
+# #
 #
 #
-# def jump_hurdle():
-#     turn_left()
-#     turn_left()
-#     turn_left()
-#     move()
-#     turn_left()
-#     turn_left()
-#     turn_left()
-#     move()
-#     turn_left()
 #
 #
-# for i in range(1, 10):
-#     move()
-#     turn_left()
-#     move()
-#     jump_hurdle()
-
-#
-# ############ RE WRITTEN CODE to include 2 functions ######
+# ################## HURDLE 4 #############
 # def turn_right():
 #     turn_left()
 #     turn_left()
 #     turn_left()
 #
 #
-# def jump_hurdle():
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
 #     turn_right()
 #     move()
 #     turn_right()
-#     move()
+#     while front_is_clear():
+#         move()
 #     turn_left()
 #
 #
-# for i in range(1, 7):
-#     move()
-#     turn_left()
-#     move()
-#     jump_hurdle()
-#
-# IN THE ABSENCE OF FOR LOOP, we could use WHILE LOOP as defined below
-#
-#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
 #
